98e2a840a960c29a0b47f6b5c6357dcae6999d93
Andre Pavanelli <andpavanelli@gmail.com>
2019-10-01 15:22:34 -0300

    Aprendendo js - fundamentos 2

---
+++ exercicios-js/fundamentos/objeto.js
@@ -0,0 +1,15 @@
+const prod1 = {} //cria um objeto vazio
+prod1.nome = 'Celular Ultra Mega'
+
+//objeto =  coleção de grupos chave-valor
+
+prod1.preco = 4998.90
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa polo',
+    preco: 79.90
+}
+
+console.log(prod2)
--- exercicios-js/fundamentos/strings.js
+++ exercicios-js/fundamentos/strings.js
@@ -3,3 +3,15 @@ const escola = "Cod3r";
 console.log(escola.charAt(4));
 
 console.log("Escola".concat(escola).concat("!"));
+
+const nome = 'Rebeca';
+const concatenacao = 'Olá ' + nome + ' !';
+const template =`
+    Olá
+    ${nome}!
+`;
+console.log(concatenacao, template);
+
+//expressões...
+
+console.log(`1 + 1 = ${1 + 1}`);
